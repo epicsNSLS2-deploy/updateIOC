b21f9666aca96b17a8f78f2193b4d73791e2f1d1
jwlodek <jwlodek@cs.stonybrook.edu>
2019-05-15 12:36:14 -0400

    added configuration file and python program to read it

---
+++ read_configure.py
@@ -0,0 +1,24 @@
+#
+# Function that reads the configure file for updating IOCs
+#
+# Author: Jakub Wlodek
+#
+
+import os
+
+def read_config():
+    result = {}
+    config_file = open("CONFIGURE", "r+")
+
+    line = config_file.readline()
+    while line:
+        line = line.strip()
+        if not line.startswith("#") and len(line) > 0:
+            pair = line.split('=')
+            result[pair[0]] = pair[1]
+        
+        line = config_file.readline()
+    
+    print("CONFIGURE file read with the following values:")
+    print(result)
+    return result
